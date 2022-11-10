7cb25bac29ee962d46ebd460eb7cbe3538e2be4a
Nozeil <nozeilwrk@gmail.com>
2022-11-10 20:34:09 +0300

    feat: add footer

---
+++ my-app/src/units/Footer/Footer.tsx
@@ -0,0 +1,68 @@
+import { Box, Container, Link, List, ListItem, Typography } from '@mui/material';
+import Grid from '@mui/system/Unstable_Grid';
+import { FC } from 'react';
+import logo from './../../assets/rs_school_js.svg';
+
+const fontSize = { xs: '14px', sm: '1rem' };
+
+type FooterListItemProps = { content: string };
+
+const FooterListItem: FC<FooterListItemProps> = ({ content }) => {
+  return (
+    <ListItem sx={{ width: 'fit-content', p: '0 5px' }}>
+      <Typography
+        component={Link}
+        href={`https://github.com/${content}`}
+        color="inherit"
+        fontSize={fontSize}
+        underline="none">
+        {content}
+      </Typography>
+    </ListItem>
+  );
+};
+
+const Footer: FC = () => {
+  return (
+    <footer>
+      <Container>
+        <Grid
+          container
+          justifyContent={{ sm: 'space-between', xs: 'center' }}
+          alignItems="center"
+          gap="10px"
+          height="60px">
+          <Grid
+            container
+            component={List}
+            justifyContent={{ xs: 'center', sm: 'flex-start' }}
+            flexGrow={1}
+            flexBasis={0}>
+            <FooterListItem content="Nozeil" />
+            <FooterListItem content="Verigota" />
+          </Grid>
+          <Grid component={Typography} fontSize={fontSize}>
+            © 2022
+          </Grid>
+          <Grid
+            container
+            component={Link}
+            href="https://rs.school/react/"
+            alignItems="center"
+            justifyContent={{ xs: 'center', sm: 'flex-end' }}
+            flexGrow={1}
+            flexBasis={0}>
+            <Box
+              component="img"
+              src={logo}
+              alt="RSSchool logo"
+              height={{ xs: '30px', sm: '40px' }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- my-app/src/units/Layout.tsx
+++ my-app/src/units/Layout.tsx
@@ -1,18 +1,20 @@
 import { Container } from '@mui/material';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from './Footer/Footer';
 import Header from './Header';
 
 const Layout: FC = () => {
   return (
-    <Container>
+    <>
       <Header />
       <main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
-    </Container>
+    </>
   );
 };
 
