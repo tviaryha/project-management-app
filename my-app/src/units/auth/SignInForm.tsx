d86115a1636c672cd4188f1448fe05e4d99ad804
Tatiana <amora4ka@gmail.com>
2022-11-12 23:01:53 +0100

    feat: add validation to SignIn form

--- my-app/src/units/auth/SignInForm.tsx
+++ my-app/src/units/auth/SignInForm.tsx
@@ -4,6 +4,17 @@ import { ISignIn } from '../../api/models/AuthInterfaces';
 import * as authFieldsNames from './AuthFieldsName';
 import { signIn } from '../../redux/signInSlice';
 import useAppDispatch from '../../hooks/useAppDispatch';
+import {
+  Box,
+  Button,
+  Container,
+  createTheme,
+  CssBaseline,
+  TextField,
+  ThemeProvider,
+  Typography
+} from '@mui/material';
+const theme = createTheme();
 
 export const SignInForm: FC = () => {
   const {
@@ -19,20 +30,68 @@ export const SignInForm: FC = () => {
   };
   return (
     <main>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>{"Let's sign in!"}</h1>
-        <label>
-          <p>Login</p>
-          <input type="text" {...register(authFieldsNames.LOGIN, { required: true })} />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" {...register(authFieldsNames.PASSWORD, { required: true })} />
-        </label>
-        <div>
-          <button type="submit">Go!</button>
-        </div>
-      </form>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}>
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{
+                marginBottom: 3
+              }}>
+              {`Let's sign in!`}
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              noValidate
+              sx={{
+                mt: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 3
+              }}>
+              <TextField
+                id="login"
+                label="Login"
+                variant="outlined"
+                required
+                type="text"
+                error={!!errors.login}
+                helperText={errors.login && errors.login.type === 'required' && 'This is required'}
+                {...register(authFieldsNames.LOGIN, {
+                  required: true,
+                  minLength: 5,
+                  maxLength: 30
+                })}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                required
+                type="password"
+                error={!!errors.password}
+                helperText={
+                  errors.password && errors.password.type === 'required' && 'This is required'
+                }
+                {...register(authFieldsNames.PASSWORD, { required: true })}
+              />
+              <Button type="submit" variant="contained">
+                Go!
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </main>
   );
 };
