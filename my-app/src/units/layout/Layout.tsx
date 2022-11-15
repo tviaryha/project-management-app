625f52ae8d3032f1cd10ba952485b2931263fec0
Tatiana <amora4ka@gmail.com>
2022-11-15 13:08:52 +0100

    feat: add loader

--- my-app/src/units/layout/Layout.tsx
+++ my-app/src/units/layout/Layout.tsx
@@ -3,17 +3,21 @@ import { Outlet } from 'react-router-dom';
 import useAppSelector from '../../hooks/useAppSelector';
 import { Toast } from '../Toast/Toast';
 import Footer from './Footer/Footer';
+import { Loader } from './Loader';
+import { Toast } from './Toast/Toast';
 import Header from './Header/Header';
 
 const Layout = () => {
   const toastMessage = useAppSelector((state) => state.toast.message);
   const toastType = useAppSelector((state) => state.toast.type);
   const toastIsOpen = useAppSelector((state) => state.toast.isOpen);
+  const isLoading = useAppSelector((state) => state.signIn.isLoading || state.signUp.isLoading);
 
   return (
     <>
       <Header />
       <Toast message={toastMessage} type={toastType} isOpen={toastIsOpen} />
+      <Loader open={isLoading || false} />
       <Box component="main" flexGrow={1}>
         <Container>
           <Outlet />
