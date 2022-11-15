c973979fb07ecb69c8d7328f8313154534299d72
Nozeil <nozeilwrk@gmail.com>
2022-11-16 01:17:28 +0300

    feat: add linear loading indicator

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,6 +14,7 @@ import MainPage from './units/pages/Main/Main';
 import NewBoard from './units/pages/NewBoard';
 import EditProfile from './units/pages/EditProfile';
 import { Suspense } from 'react';
+import LinearLoadingIndicator from './components/LinearLoadingIndicator';
 
 const { base, signIn, signUp, mainPage, newBoard, editProfile } = Paths;
 
@@ -31,7 +32,7 @@ const router = createBrowserRouter(
 );
 
 const App = () => (
-  <Suspense fallback="loading">
+  <Suspense fallback={<LinearLoadingIndicator />}>
     <RouterProvider router={router} />
   </Suspense>
 );
