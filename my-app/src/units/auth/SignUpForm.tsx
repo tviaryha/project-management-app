98e8cbdcefe9232d43a04f8f09568fbc4ac6f18c
Tatiana <amora4ka@gmail.com>
2022-11-18 09:57:02 +0100

    fix: resolve localisation issues in sign forms

--- my-app/src/units/auth/SignUpForm.tsx
+++ my-app/src/units/auth/SignUpForm.tsx
@@ -75,114 +75,111 @@ export const SignUpForm: FC = () => {
   };
 
   return (
-    <main>
-      <Container component="main" maxWidth="xs">
-        <Box
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+        <Typography
+          component="h1"
+          variant="h5"
           sx={{
-            marginTop: 8,
+            marginBottom: 3
+          }}>
+          {t(signUpTitle)}
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          sx={{
+            mt: 1,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center'
+            alignItems: 'center',
+            gap: 3
           }}>
-          <Typography
-            component="h1"
-            variant="h5"
+          <TextField
+            id="name"
+            label={t(name)}
+            variant="outlined"
+            required
+            type="text"
             sx={{
-              marginBottom: 3
-            }}>
-            {t(signUpTitle)}
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
+              width: '50ch'
+            }}
+            error={!!errors.name}
+            helperText={t(errors.name?.message || '')}
+            {...register(AuthFieldsNames.NAME, {
+              required: { value: true, message: requiredE },
+              minLength: { value: 3, message: minLength3E },
+              maxLength: { value: 30, message: maxLength30E }
+            })}
+          />
+          <TextField
+            id="login"
+            label={t(login)}
+            variant="outlined"
+            required
+            type="text"
             sx={{
-              mt: 1,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              gap: 3
+              width: '50ch'
+            }}
+            error={!!errors.login}
+            helperText={t(errors.login?.message || '')}
+            {...register(AuthFieldsNames.LOGIN, {
+              required: { value: true, message: requiredE },
+              minLength: { value: 3, message: minLength3E },
+              maxLength: { value: 30, message: maxLength30E }
+            })}
+          />
+          <TextField
+            id="password"
+            label={t(password)}
+            variant="outlined"
+            required
+            type="password"
+            sx={{
+              width: '50ch'
+            }}
+            error={!!errors.password}
+            helperText={t(errors.password?.message || '')}
+            {...register(AuthFieldsNames.PASSWORD, {
+              validate: (value) =>
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/.test(value) || passwordPatternE,
+              required: { value: true, message: requiredE }
+            })}
+          />
+          <TextField
+            id="confirm-password"
+            label={t(confirmPassword)}
+            variant="outlined"
+            required
+            type="password"
+            sx={{
+              width: '50ch'
+            }}
+            error={!!errors.confirm_password}
+            helperText={t(errors.confirm_password?.message || '')}
+            {...register(AuthFieldsNames.CONFIRM_PASSWORD, {
+              validate: (value) =>
+                value === getValues(AuthFieldsNames.PASSWORD) || confirmPasswordE,
+              required: { value: true, message: requiredE }
+            })}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              width: '25ch'
             }}>
-            <TextField
-              id="name"
-              label={t(name)}
-              variant="outlined"
-              required
-              type="text"
-              sx={{
-                width: '50ch'
-              }}
-              error={!!errors.name}
-              helperText={errors.name?.message}
-              {...register(AuthFieldsNames.NAME, {
-                required: { value: true, message: t(requiredE) },
-                minLength: { value: 3, message: t(minLength3E) },
-                maxLength: { value: 30, message: t(maxLength30E) }
-              })}
-            />
-            <TextField
-              id="login"
-              label={t(login)}
-              variant="outlined"
-              required
-              type="text"
-              sx={{
-                width: '50ch'
-              }}
-              error={!!errors.login}
-              helperText={errors.login?.message}
-              {...register(AuthFieldsNames.LOGIN, {
-                required: { value: true, message: t(requiredE) },
-                minLength: { value: 3, message: t(minLength3E) },
-                maxLength: { value: 30, message: t(maxLength30E) }
-              })}
-            />
-            <TextField
-              id="password"
-              label={t(password)}
-              variant="outlined"
-              required
-              type="password"
-              sx={{
-                width: '50ch'
-              }}
-              error={!!errors.password}
-              helperText={errors.password?.message}
-              {...register(AuthFieldsNames.PASSWORD, {
-                validate: (value) =>
-                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/.test(value) ||
-                  t<string>(passwordPatternE),
-                required: { value: true, message: t(requiredE) }
-              })}
-            />
-            <TextField
-              id="confirm-password"
-              label={t(confirmPassword)}
-              variant="outlined"
-              required
-              type="password"
-              sx={{
-                width: '50ch'
-              }}
-              error={!!errors.confirm_password}
-              helperText={errors.confirm_password?.message}
-              {...register(AuthFieldsNames.CONFIRM_PASSWORD, {
-                validate: (value) =>
-                  value === getValues(AuthFieldsNames.PASSWORD) || t<string>(confirmPasswordE),
-                required: { value: true, message: t(requiredE) }
-              })}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                width: '25ch'
-              }}>
-              {t(btn)}
-            </Button>
-          </Box>
+            {t(btn)}
+          </Button>
         </Box>
-      </Container>
-    </main>
+      </Box>
+    </Container>
   );
 };
