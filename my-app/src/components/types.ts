b0f15b5366968c5353e388223cad0530f96a12c5
Olga Polyanskaya <83548013+OlyaPolya@users.noreply.github.com>
2022-11-23 03:32:20 +0700

    feat: implement task form

---
+++ my-app/src/components/types.ts
@@ -0,0 +1,10 @@
+export interface ITask {
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+  columnId: string;
+  description: string;
+  userId: string;
+  users: string[];
+}
--- my-app/src/units/pages/Main/Main.tsx
+++ my-app/src/units/pages/Main/Main.tsx
@@ -7,3 +7,27 @@ const Main = () => {
 };
 
 export default Main;
+
+// const TaskProps = {
+//   _id: 'id',
+//   title: 'titlebbbbbbbbbbbbb',
+//   order: 1,
+//   boardId: 'boardId',
+//   columnId: 'columnId',
+//   description: 'descriptionbbbbbbbbbbbbbbbbbb',
+//   userId: 'userId',
+//   users: ['userId']
+// };
+
+// const Main = () => {
+//   useCloseMenu();
+
+//   return (
+//     <>
+//       <Task {...TaskProps} />
+//       <div>MainPage</div>
+//     </>
+//   );
+// };
+
+// export default Main;
