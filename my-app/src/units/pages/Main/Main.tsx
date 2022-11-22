e44356f7072f15ead4ed52541810d75fe3200a99
Tatiana <amora4ka@gmail.com>
2022-11-22 01:12:35 +0100

    feat: add main page component

--- my-app/src/units/pages/Main/Main.tsx
+++ my-app/src/units/pages/Main/Main.tsx
@@ -1,9 +1,41 @@
+import { Grid } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LinearLoadingIndicator from '../../../components/LinearLoadingIndicator';
+import { ErrorCodes, LocalStorageKeys, Paths } from '../../../enums';
+import useAppDispatch from '../../../hooks/useAppDispatch';
+import useAppSelector from '../../../hooks/useAppSelector';
 import useCloseMenu from '../../../hooks/useCloseMenu';
+import { loadUserBoards } from '../../../redux/boardsListSlice';
 
 const Main = () => {
   useCloseMenu();
 
-  return <div>MainPage</div>;
+  const { isLoading, boards } = useAppSelector((state) => state.boardsList);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const getUserBoards = async () => {
+    const userId = localStorage.getItem(LocalStorageKeys.userId);
+
+    if (userId) {
+      try {
+        await dispatch(loadUserBoards(userId)).unwrap();
+      } catch (e) {
+        if (e === ErrorCodes.NOT_FOUND) {
+          navigate(Paths.error);
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    getUserBoards();
+  }, []);
+
+  if (isLoading) {
+    return <LinearLoadingIndicator />;
+  }
+  return <Grid container component="section" justifyContent="space-evenly" gap="20px"></Grid>;
 };
 
 export default Main;
