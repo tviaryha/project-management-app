5f54816b40505fd3cc4ef7ebf5afc7906a0bf7f0
Nozeil <nozeilwrk@gmail.com>
2022-11-23 14:04:27 +0300

    fix: add ns field to i18n

--- my-app/src/i18n.ts
+++ my-app/src/i18n.ts
@@ -2,12 +2,21 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import { TranslationKeys as EditProfileTK } from './units/pages/EditProfile/enum';
+import { TranslationKeys as ToastTK } from './units/Toast/enum';
+import { TranslationKeys as NewBoardTK } from './units/pages/NewBoard/enum';
+import { TranslationKeys as HeaderTK } from './units/layout/Header/enums';
+import { TranslationKeys as ErrorPageTK } from './units/pages/ErrorPage/enum';
+import { FormTranslationKeys as FormTK } from './enums';
+
+const ns = [EditProfileTK.ns, ToastTK.ns, NewBoardTK.ns, HeaderTK.ns, ErrorPageTK.ns, FormTK.ns];
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    ns,
     fallbackLng: 'en',
     debug: false,
     detection: {
